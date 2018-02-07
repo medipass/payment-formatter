ae20d8d3c6146ad61f02654c9f7b270b30453438
jxom <jakemoxey@gmail.com>
2018-02-08 10:56:39 +1100

    Add rollup minify config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,33 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import {minify} from 'uglify-es';
 
-export default {
-  input: `${__dirname}/src/index.js`,
-  output: {
-    name: 'paymentFormatter',
-    file: `${__dirname}/umd/index.js`,
-    format: 'umd'
+export default [
+  {
+    input: `${__dirname}/src/index.js`,
+    output: {
+      name: 'paymentFormatter',
+      file: `${__dirname}/umd/index.js`,
+      format: 'umd'
+    },
+    plugins: [
+      babel({
+        presets: [['env', {modules: false}]]
+      })
+    ]
   },
-  plugins: [
-    babel({
-      presets: [['env', {modules: false}]]
-    })
-  ]
-};
+  {
+    input: `${__dirname}/src/index.js`,
+    output: {
+      name: 'paymentFormatter',
+      file: `${__dirname}/umd/index.min.js`,
+      format: 'umd'
+    },
+    plugins: [
+      babel({
+        presets: [['env', {modules: false}]]
+      }),
+      uglify({}, minify)
+    ]
+  }
+];
